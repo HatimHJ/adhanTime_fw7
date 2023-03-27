efbd12dd2fbe01040db75ff47ae6ff75c46a5297
HatimHJ <98179443+HatimHJ@users.noreply.github.com>
2023-03-27 10:19:08 +0300

    SRP

--- js/my-app.js
+++ js/my-app.js
@@ -1,39 +1,18 @@
 var $$ = Dom7;
 var coords;
-
-async function getAdhanTime(coords) {
-	const { lat, lon } = coords;
-	const year = new Date().getFullYear().toString();
-	const method = "4";
-	const baseUrl = "https://api.aladhan.com/v1/calendar";
-	const url = `${baseUrl}/${year}?latitude=${lat}&longitude=${lon}&method=${method}`;
-	const res = await fetch(url);
-	console.log(res);
-	return res;
-}
+var year = new Date().getFullYear().toString();
+var method = "4";
 
 $$(document).on("pageInit", function (e) {
 	if (e.detail.page.name === "index") {
-		console.log(navigator.permissions);
-
 		navigator.geolocation.getCurrentPosition(
-			async (pos) => {
-				coords = { lat: pos.coords.latitude, lon: pos.coords.longitude };
-				const cityName = await CoordToCity(coords);
-				handleFetchData(cityName, coords);
-			},
-			async () => {
-				coords = { lat: 21.51694, lon: 39.21917 };
-				const cityName = await CoordToCity(coords);
-				handleFetchData(cityName, coords);
-			}
+			(pos) => init(pos.coords.latitude, pos.coords.longitude, year, method),
+			() => init(21.51694, 39.21917, year, method)
 		);
 
-		$$("#search-btn").on("click", async function () {
-			handleInput();
-		});
-		$$("#search-input").on("keyup", async function (e) {
-			// todo
+		$$("#search-btn").on("click", () => {
+			preloading();
+			handleInput(year, method);
 		});
 	}
 });
@@ -45,6 +24,12 @@ var myApp = new Framework7();
 var mainView = myApp.addView(".view-main", {
 	dynamicNavbar: true,
 });
+
+async function init(lat, lon, year, method) {
+	const coords = { lat, lon };
+	const cityName = await CoordToCity(coords);
+	handleFetchData(cityName, coords, year, method);
+}
 
 async function cityToCoord(city) {
 	const url = "https://geocode.maps.co/search?q=" + city;
@@ -64,27 +49,74 @@ async function CoordToCity(coord) {
 	return city;
 }
 
-function displayData(adhanData, city) {
+function formatData(adhanData) {
 	const month = new Date().getMonth() + 1;
 	const day = new Date().getDate() - 1;
 	const { timings, date } = adhanData.data[month][day];
 	const { Fajr, Dhuhr, Asr, Maghrib, Isha } = timings;
 	const pryerTime = [Fajr, Dhuhr, Asr, Maghrib, Isha];
+	return { date, pryerTime };
+}
+
+function convertTimeToNumber(time) {
+	const [hrs, min] = time.split(":");
+	const number = Number(`${hrs}.${min}`);
+	return number;
+}
+
+function prepareNextPryer(currentHrs, currentMin, pryerTime) {
+	const formatMin = currentMin < 10 ? `0${currentMin}` : currentMin;
+	const currentTime = Number(`${currentHrs}.${formatMin}`);
+	const time = pryerTime[pryerTime.length - 1].split(" ")[0];
+	const nightTime = convertTimeToNumber(time);
+	return { currentTime, nightTime, pryerTime };
+}
+
+function getNextPryer(currentTime, nightTime, pryerTime) {
+	if (currentTime > nightTime) {
+		return pryerTime[0];
+	} else {
+		for (let i = 0; i < pryerTime.length; i++) {
+			const time = pryerTime[i].split(" ")[0];
+			const iterationTime = convertTimeToNumber(time);
+			if (iterationTime > currentTime) return pryerTime[i];
+		}
+	}
+}
+
+function manageData(adhanData) {
+	const { date, pryerTime } = formatData(adhanData);
+	const currentHrs = new Date().getHours();
+	const currentMin = new Date().getMinutes();
+	const { currentTime, nightTime } = prepareNextPryer(
+		currentHrs,
+		currentMin,
+		pryerTime
+	);
+	const next = getNextPryer(currentTime, nightTime, pryerTime);
+	return { date, pryerTime, next };
+}
+
+function displayData(adhanData, city) {
+	const { date, pryerTime, next } = manageData(adhanData);
 	$$(".result .times .time").each(function (i) {
 		$$(this).text(pryerTime[i]);
+		if ($$(this)[0].innerHTML == next) $$(this).parent().addClass("next");
 	});
 	$$(".date").text(date.readable);
 	$$(".city").text(city);
 	$$(".loading").remove();
 }
 
-async function handleFetchData(cityName, coords) {
+function outputTheData() {}
+
+async function handleFetchData(cityName, coords, year, method) {
 	try {
-		const adhanData = await getAdhanTime(coords);
+		const adhanData = await getAdhanTime(coords, year, method);
 		displayData(await adhanData.json(), cityName);
 	} catch (error) {
 		const time = setInterval(async () => {
-			const adhanData = await getAdhanTime(coords);
+			const adhanData = await getAdhanTime(coords, year, method);
 			if (adhanData.ok == true) {
 				clearInterval(time);
 				displayData(await adhanData.json(), cityName);
@@ -93,13 +125,24 @@ async function handleFetchData(cityName, coords) {
 	}
 }
 
-async function handleInput() {
+function preloading() {
 	const preloader = `<div class="loading">
 								<img src="./img/favicon-32x32.png" alt="" />
 								<p>loading</p>
 							</div>`;
 	$$(".wrapper").prepend(preloader);
+}
+
+async function handleInput(year, method) {
 	const cityName = $$("#search-input").val();
 	coords = await cityToCoord(cityName);
-	handleFetchData(cityName, coords);
+	handleFetchData(cityName, coords, year, method);
+}
+
+async function getAdhanTime(coords, year, method) {
+	const { lat, lon } = coords;
+	const baseUrl = "https://api.aladhan.com/v1/calendar";
+	const url = `${baseUrl}/${year}?latitude=${lat}&longitude=${lon}&method=${method}`;
+	const res = await fetch(url);
+	return res;
 }
