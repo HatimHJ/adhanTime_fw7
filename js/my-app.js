ddcf454f1fcf25360e40a505a42a3824d1d82fa1
HatimHJ <98179443+HatimHJ@users.noreply.github.com>
2023-03-28 23:17:55 +0300

    cleaning up, config page

--- js/my-app.js
+++ js/my-app.js
@@ -1,10 +1,9 @@
 var $$ = Dom7;
 var coords;
-var year = new Date().getFullYear().toString();
-var method = "4";
 
 $$(document).on("pageInit", function (e) {
 	if (e.detail.page.name === "index") {
+		const { year, method } = getYearAndMethod();
 		navigator.geolocation.getCurrentPosition(
 			(pos) => init(pos.coords.latitude, pos.coords.longitude, year, method),
 			() => init(21.51694, 39.21917, year, method)
@@ -20,6 +19,56 @@ $$(document).on("pageInit", function (e) {
 // Initialize your app
 var myApp = new Framework7();
 
+function addToLocalStorage(data) {
+	localStorage.setItem("year", data.year);
+	localStorage.setItem("method", data.method);
+}
+
+function removeFromLocalStorage() {
+	const itemsToRemove = ["year", "method"];
+	if (itemsToRemove.length) {
+		for (let i = 0; i < itemsToRemove.length; i++)
+			localStorage.removeItem(itemsToRemove[i]);
+	}
+}
+
+// not tested....
+function counter(current, next) {
+	const month = new Date().getMonth() + 1;
+	const day = new Date().getDate() - 1;
+	const year = new Date().getFullYear();
+	const currentTime = getHrsAndMin(current);
+	const nextTime = getHrsAndMin(next);
+	var dateS = new Date(
+		`${month}/${day}/${year} ${currentTime[0]}:${currentTime[1]}`
+	);
+	var dateE = new Date(`${month}/${day}/${year} ${nextTime[0]}:${nextTime[1]}`);
+
+	var diff = dateE.getTime() - dateS.getTime();
+	console.log(diff);
+	return diff;
+}
+
+// not tested....
+function getNotification(interval) {
+	const end = setInterval(() => {
+		console.log("ggg");
+	}, interval);
+}
+
+// config page
+myApp.onPageInit("config", () => {
+	$$("#submit").on("click", () => {
+		const data = myApp.formGetData("my-form");
+		addToLocalStorage(data);
+		mainView.router.loadPage("index.html");
+	});
+	$$("#reset").on("click", () => {
+		myApp.formDeleteData("my-form");
+		removeFromLocalStorage();
+		mainView.router.loadPage("index.html");
+	});
+});
 // Add view
 var mainView = myApp.addView(".view-main", {
 	dynamicNavbar: true,
@@ -59,9 +108,20 @@ function formatData(adhanData) {
 }
 
 function convertTimeToNumber(time) {
+	if (time.includes(" ")) time = time.split(" ")[0];
 	const [hrs, min] = time.split(":");
 	const number = Number(`${hrs}.${min}`);
 	return number;
+}
+
+// time should be as 21:21
+function getHrsAndMin(time) {
+	const sep = time.toString()[2]; //very risky
+	let timeString = time.toString();
+	if (timeString.includes(" ")) timeString = timeString.split(" ")[0];
+
+	const [hrs, min] = timeString.split(sep);
+	return [hrs, min];
 }
 
 function prepareNextPryer(currentHrs, currentMin, pryerTime) {
@@ -73,14 +133,11 @@ function prepareNextPryer(currentHrs, currentMin, pryerTime) {
 }
 
 function getNextPryer(currentTime, nightTime, pryerTime) {
-	if (currentTime > nightTime) {
-		return pryerTime[0];
-	} else {
-		for (let i = 0; i < pryerTime.length; i++) {
-			const time = pryerTime[i].split(" ")[0];
-			const iterationTime = convertTimeToNumber(time);
-			if (iterationTime > currentTime) return pryerTime[i];
-		}
+	if (currentTime > nightTime) return pryerTime[0];
+	for (let i = 0; i < pryerTime.length; i++) {
+		const time = pryerTime[i].split(" ")[0];
+		const iterationTime = convertTimeToNumber(time);
+		if (iterationTime > currentTime) return pryerTime[i];
 	}
 }
 
@@ -94,6 +151,8 @@ function manageData(adhanData) {
 		pryerTime
 	);
 	const next = getNextPryer(currentTime, nightTime, pryerTime);
+	const interval = counter(currentTime, next);
+	getNotification(interval);
 	return { date, pryerTime, next };
 }
 
@@ -101,14 +160,13 @@ function displayData(adhanData, city) {
 	const { date, pryerTime, next } = manageData(adhanData);
 	$$(".result .times .time").each(function (i) {
 		$$(this).text(pryerTime[i]);
+		$$(this).parent().removeClass("next");
 		if ($$(this)[0].innerHTML == next) $$(this).parent().addClass("next");
 	});
 	$$(".date").text(date.readable);
 	$$(".city").text(city);
 	$$(".loading").remove();
 }
-
-function outputTheData() {}
 
 async function handleFetchData(cityName, coords, year, method) {
 	try {
@@ -145,4 +203,14 @@ async function getAdhanTime(coords, year, method) {
 	const url = `${baseUrl}/${year}?latitude=${lat}&longitude=${lon}&method=${method}`;
 	const res = await fetch(url);
 	return res;
+}
+
+function getYearAndMethod() {
+	const defaultYear = new Date().getFullYear().toString();
+	const defaultMethod = "4";
+	const storedYear = localStorage.getItem("year");
+	const storedMethod = localStorage.getItem("method");
+	const year = storedYear ? storedYear : defaultYear;
+	const method = storedMethod ? storedMethod : defaultMethod;
+	return { year, method };
 }
